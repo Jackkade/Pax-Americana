f5821390b1b42633f705465a29830d6bcd74260e
Jackkade <40548166+Mmklop@users.noreply.github.com>
2024-09-30 21:10:00 -0500

    Changed Colony upkeep back to 2 ducats

--- common/defines/pa_defines.lua
+++ common/defines/pa_defines.lua
@@ -2,4 +2,4 @@
 NDefines.NCountry.MAX_CROWN_COLONIES = 9 --Probably Temporary
 NDefines.NEconomy.TRADE_WIND_STRENGTH = 0.6 -- Made slightly more effective, still largely inconsiquensal
 NDefines.NCountry.REGULAR_COLONY_GROWTH = 50 -- Doubled to 50, may be uneeded.
-NDefines.NEconomy.COLONIAL_MAINTENANCE_FACTOR = 12.0 -- Made more expensive
+NDefines.NEconomy.COLONIAL_MAINTENANCE_FACTOR = 8.0 -- Reverted to vanilla
