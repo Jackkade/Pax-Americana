cf8b0ee71b460a4cb83bd708ee233ed219cd1800
Jackkade <mmklop.mmklop@gmail.com>
2025-03-30 20:34:49 -0500

    Lowered Colonial Maintnance back to normal

--- common/defines/pa_defines.lua
+++ common/defines/pa_defines.lua
@@ -2,7 +2,7 @@
 NDefines.NCountry.MAX_CROWN_COLONIES = 9 --Probably Temporary
 NDefines.NEconomy.TRADE_WIND_STRENGTH = 0.6 -- Made slightly more effective, still largely inconsiquensal
 NDefines.NCountry.REGULAR_COLONY_GROWTH = 20 -- Doubled to 50, may be uneeded.
-NDefines.NEconomy.COLONIAL_MAINTENANCE_FACTOR = 12.0 -- 
+NDefines.NEconomy.COLONIAL_MAINTENANCE_FACTOR = 8.0 -- 
 NDefines.NCountry.PS_ADD_TRIBAL_LAND = 300
 NDefines.NCountry.PS_ADD_TRIBAL_LAND_EXTRA_COST_PER_PROVINCE = 100
 NDefines.NCountry.TRIBE_SETTLE_ADM_COST = 150
